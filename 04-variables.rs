ce3c8fc4841fae227e28d5f2b9e10407bcc351f4
gabriele.lana <gabriele.lana@cleancode.it>
2014-11-14 22:22:16 +0100

    04 - variables

---
+++ 04-variables.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let an_integer = 1u;
+    let a_boolean = true;
+    let unit = ();
+
+    // copy `an_integer` into `copied_integer`
+    let copied_integer = an_integer;
+
+    println!("An integer: {}", copied_integer);
+    println!("A boolean: {}", a_boolean);
+    println!("Meet the unit value: {}", unit);
+
+    // The compiler warns about unused variables; these warnings can be
+    // silenced by prefixing the variable name with an underscore
+    let _unused_variable = 3u;
+}
