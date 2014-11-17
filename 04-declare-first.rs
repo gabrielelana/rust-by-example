249e9d262ae801aec0158cfe95c49a0d8cbf89b9
gabriele.lana <gabriele.lana@cleancode.it>
2014-11-17 23:17:20 +0100

    04 - declare first

---
+++ 04-declare-first.rs
@@ -0,0 +1,22 @@
+fn main() {
+    // Declare a variable
+    let a_variable;
+
+    {
+        let x = 2i;
+
+        // Initialize the variable
+        a_variable = x * x;
+    }
+
+    println!("a variable: {}", a_variable);
+
+    let another_variable;
+
+    // Error! Use of uninitialized variable
+    // println!("another variable: {}", another_variable);
+
+    another_variable = 1i;
+
+    println!("another variable: {}", another_variable);
+}
