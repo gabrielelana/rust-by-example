1dd84f9ba38d0a96d6c50aeb9c76bd22a514abe3
gabriele.lana <gabriele.lana@cleancode.it>
2014-11-19 23:40:19 +0100

    05 - casting

---
+++ 05-casting.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let decimal = 65.4321_f32;
+
+    // Error! No implicit conversion
+    // let integer: u8 = decimal;
+
+    // Explicit conversion
+    let integer = decimal as u8;
+    let character = integer as char;
+
+    println!("Casting: {} -> {} -> {}", decimal, integer, character);
+}
